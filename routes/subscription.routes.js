2485a54cf7b715c8a230a389a0e6fb71a1088e6e
Ayush Shrivastava <ayushshrivastava840@gmail.com>
2025-02-17 14:18:30 +0530

    Nodemailer done

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import { createSubscription, getUserSubscriptions } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
@@ -10,9 +12,7 @@ subscriptionRouter.get('/:id',(req,res)=> res.send( {
     title: 'Get subscription by id'
 }));
 
-subscriptionRouter.post('/',(req,res)=> res.send( {
-    title: 'Create new subscription'
-}));
+subscriptionRouter.post('/', authorize,createSubscription);
 
 subscriptionRouter.put('/:id',(req,res)=> res.send( {
     title: 'Update subscription by id'
@@ -22,9 +22,7 @@ subscriptionRouter.delete('/:id',(req,res)=> res.send( {
     title: 'Delete subscription by id'
 }));
 
-subscriptionRouter.get('/user/:id',(req,res)=> res.send({
-    title: 'Get all User Subacriptions by id'
-}))
+subscriptionRouter.get('/user/:id',authorize,getUserSubscriptions)
 
 subscriptionRouter.put('/:id/cancel',(req,res)=>res.send({
     title: 'Cancel subscription by id'
